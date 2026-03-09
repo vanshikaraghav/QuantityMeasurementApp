34ea7d8ef34f1c6bdfb0ae9b9b5d9155a5bd3dbd
vanshikaraghav <134064972+vanshikaraghav@users.noreply.github.com>
2026-03-09 11:46:09 +0530

    Refactor FeetMeasurement tests to use new class

--- src/test/java/QuantityMeasurement/QuanityMeasurementApp/FeetMeasurementTest.java
+++ src/test/java/QuantityMeasurement/QuanityMeasurementApp/FeetMeasurementTest.java
@@ -8,32 +8,32 @@ public class FeetMeasurementTest {
     // UC1 - Feet Tests
     @Test
     void testFeetEquality_SameValue() {
-        UC1FeetMeasurement.Feet f1 = new UC1FeetMeasurement.Feet(1.0);
-        UC1FeetMeasurement.Feet f2 = new UC1FeetMeasurement.Feet(1.0);
+        FeetMeasurement.Feet f1 = new FeetMeasurement.Feet(1.0);
+        FeetMeasurement.Feet f2 = new FeetMeasurement.Feet(1.0);
         assertTrue(f1.equals(f2));
     }
 
     @Test
     void testFeetEquality_DifferentValue() {
         assertFalse(
-            new UC1FeetMeasurement.Feet(1.0)
-                .equals(new UC1FeetMeasurement.Feet(2.0))
+            new FeetMeasurement.Feet(1.0)
+                .equals(new FeetMeasurement.Feet(2.0))
         );
     }
 
     @Test
     void testFeetEquality_Null() {
-        assertFalse(new UC1FeetMeasurement.Feet(1.0).equals(null));
+        assertFalse(new FeetMeasurement.Feet(1.0).equals(null));
     }
 
     @Test
     void testFeetEquality_TypeMismatch() {
-        assertFalse(new UC1FeetMeasurement.Feet(1.0).equals("abc"));
+        assertFalse(new FeetMeasurement.Feet(1.0).equals("abc"));
     }
 
     @Test
     void testFeetEquality_SameReference() {
-        UC1FeetMeasurement.Feet f = new UC1FeetMeasurement.Feet(1.0);
+        FeetMeasurement.Feet f = new FeetMeasurement.Feet(1.0);
         assertTrue(f.equals(f));
     }
 
@@ -41,33 +41,33 @@ public class FeetMeasurementTest {
     @Test
     void testInchEquality_SameValue() {
         assertTrue(
-            new UC1FeetMeasurement.Inches(5.0)
-                .equals(new UC1FeetMeasurement.Inches(5.0))
+            new FeetMeasurement.Inches(5.0)
+                .equals(new FeetMeasurement.Inches(5.0))
         );
     }
 
     @Test
     void testInchEquality_DifferentValue() {
         assertFalse(
-            new UC1FeetMeasurement.Inches(5.0)
-                .equals(new UC1FeetMeasurement.Inches(6.0))
+            new FeetMeasurement.Inches(5.0)
+                .equals(new FeetMeasurement.Inches(6.0))
         );
     }
 
     @Test
     void testInchEquality_Null() {
-        assertFalse(new UC1FeetMeasurement.Inches(5.0).equals(null));
+        assertFalse(new FeetMeasurement.Inches(5.0).equals(null));
     }
 
     @Test
     void testInchEquality_TypeMismatch() {
-        assertFalse(new UC1FeetMeasurement.Inches(5.0)
-                .equals(new UC1FeetMeasurement.Feet(5.0)));
+        assertFalse(new FeetMeasurement.Inches(5.0)
+                .equals(new FeetMeasurement.Feet(5.0)));
     }
 
     @Test
     void testInchEquality_SameReference() {
-        UC1FeetMeasurement.Inches i = new UC1FeetMeasurement.Inches(5.0);
+        FeetMeasurement.Inches i = new FeetMeasurement.Inches(5.0);
         assertTrue(i.equals(i));
     }
 }
