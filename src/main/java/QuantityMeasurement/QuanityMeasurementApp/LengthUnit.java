2f1057c6f6a2dde5a557aa2534dfcfb77f33b786
vanshika raghav <vanshikathakur46@gmail.com>
2026-03-10 12:03:34 +0530

    [Vanshika] Weight Measurement

---
+++ src/main/java/QuantityMeasurement/QuanityMeasurementApp/LengthUnit.java
@@ -0,0 +1,29 @@
+package QuantityMeasurement.QuanityMeasurementApp;
+
+public enum LengthUnit {
+
+    FEET(1.0),
+    INCHES(1.0 / 12.0),
+    YARDS(3.0),
+    CENTIMETERS(1.0 / 30.48);
+
+    private final double conversionFactorToFeet;
+
+    LengthUnit(double factor) {
+        this.conversionFactorToFeet = factor;
+    }
+
+    public double getConversionFactor() {
+        return conversionFactorToFeet;
+    }
+
+    // convert this unit - base unit (feet)
+    public double convertToBaseUnit(double value) {
+        return value * conversionFactorToFeet;
+    }
+
+    // convert base unit (feet) - this unit
+    public double convertFromBaseUnit(double baseValue) {
+        return baseValue / conversionFactorToFeet;
+    }
+}
---
+++ src/main/java/QuantityMeasurement/QuanityMeasurementApp/WeightUnit.java
@@ -0,0 +1,26 @@
+package QuantityMeasurement.QuanityMeasurementApp;
+
+public enum WeightUnit {
+
+    KILOGRAM(1.0),
+    GRAM(0.001),
+    POUND(0.453592);
+
+    private final double conversionFactor;
+
+    WeightUnit(double conversionFactor) {
+        this.conversionFactor = conversionFactor;
+    }
+
+    public double getConversionFactor() {
+        return conversionFactor;
+    }
+
+    public double convertToBaseUnit(double value) {
+        return value * conversionFactor;
+    }
+
+    public double convertFromBaseUnit(double baseValue) {
+        return baseValue / conversionFactor;
+    }
+}
